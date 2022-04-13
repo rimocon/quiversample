4f3af40d792f0b8f01a9cd51b5012a72a5ec593a
rimocon <ttyd4343@gmail.com>
2022-04-13 20:16:41 +0900

    first commit

---
+++ quiv.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+XL = 5
+YL = 5
+x = np.arange(-XL,XL,1)
+y = np.arange(-YL,YL,1)
+X,Y = np.meshgrid(x,y)
+plt.figure()
+a = np.array([[0,0],
+              [-3,0]])
+
+U = a[0][0] * X + a[0][1] * Y
+V = a[1][0] * X + a[1][1] * Y
+
+plt.quiver(X,Y,U,V,color='black',angles='xy',scale_units='xy', scale=4.5)
+plt.xlim(-XL,XL)
+plt.ylim(-YL,YL)
+plt.savefig("1/i.png")
+plt.show()
